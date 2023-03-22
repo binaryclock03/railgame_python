69719e750dd87e2bbbefd0e8e9d5e17da1d80206
Daniel <binaryclock03@gmail.com>
2023-03-21 23:15:44 -0400

    moved menu assets into menu subdir

--- scenes/scene_main_menu.py
+++ scenes/scene_main_menu.py
@@ -8,12 +8,12 @@ def run_scene(screen):
     # instantiating buttons
     buttons:dict = {}
     buttons_x = 300
-    buttons.update({"New Game" : Button(buttons_x, 340, load_image("new_game_red.png"), 6)})
-    buttons.update({"Load Game" : Button(buttons_x, 440, load_image("load_game_red.png"), 6, image_hover=load_image("load_game_yel.png"))})
-    buttons.update({"Settings" : Button(buttons_x, 540, load_image("settings_red.png"), 6)})
-    #buttons.update({"Quit Game" : Button(buttons_x, 160, load_image("Quit_Game.png"), 8)})
+    buttons.update({"New Game" :  Button(buttons_x, 340, load_image("menu/new_game_red.png"),  6, image_hover=load_image("menu/new_game_yel.png"))})
+    buttons.update({"Load Game" : Button(buttons_x, 440, load_image("menu/load_game_red.png"), 6, image_hover=load_image("menu/load_game_yel.png"))})
+    buttons.update({"Settings" :  Button(buttons_x, 540, load_image("menu/settings_red.png"),  6, image_hover=load_image("menu/settings_yel.png"))})
+    buttons.update({"Quit" :      Button(buttons_x, 640, load_image("menu/quit_red.png"),      6, image_hover=load_image("menu/quit_yel.png"))})
 
-    title = load_image("Title.png", scale = 4)
+    title = load_image("menu/Title.png", scale = 4)
 
     while running:
         # draw background
@@ -29,7 +29,8 @@ def run_scene(screen):
             print("Load")
         if buttons.get("Settings").draw(screen):
             print("Settings")
-
+        if buttons.get("Quit").draw(screen):
+            sys.exit()
 
         for event in pg.event.get():
             if event.type == pg.QUIT: sys.exit()
