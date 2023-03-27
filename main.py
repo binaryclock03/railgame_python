77d20452100a617c7785cbbb97aa9b5d3515c9cb
Daniel <binaryclock03@gmail.com>
2023-03-27 16:34:02 -0400

    pushing splines to spline branch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame as pg
 import scenes.scene_main_menu as scene_main_menu
 import scenes.scene_generator as scene_generator
+import scenes.scene_simulation as scene_simulation
 
 # pygame stuff
 pg.init()
@@ -8,10 +9,12 @@ SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 1200,800
 screen = pg.display.set_mode(SCREEN_SIZE)
 
 # game variables
-scene = "main_menu"
+scene = "simulator"
 
 while True:
     if scene == "main_menu":
         scene = scene_main_menu.run_scene(screen)
     elif scene == "generator":
         scene = scene_generator.run_scene(screen)
+    elif scene == "simulator":
+        scene = scene_simulation.run_scene(screen)
